d1296426f717250b98ca43e45b01bae1df108de9
Maiko André <ewigemagie@outlook.com>
2026-05-16 15:28:16 -0300

    feat: implement the functions to extract the data of  "uso_solo" and "producao_agricola"

--- etl/extract.py
+++ etl/extract.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import sidrapy
 
-def extract_data():
+def extract_populacao():
     sidra = sidrapy.get_table(
         table_code='9514', 
         territorial_level='6',
@@ -10,5 +10,37 @@ def extract_data():
     )
     return sidra
 
-raw_data = extract_data()
-raw_data.head()
+def extract_producao_agricola():
+    sidra = sidrapy.get_table(
+        table_code='839',
+        territorial_level='6',
+        variable='214',
+        ibge_territorial_code='all'
+    )
+    return sidra
+
+def extract_producao_pecuaria():
+    pass
+
+def extract_biomas():
+    pass
+
+def extract_uso_solo():
+    sidra = sidrapy.get_table(
+        table_code='7319',
+        territorial_level='123',
+        variable='10473',
+        ibge_territorial_code='all'
+    )
+    return sidra
+
+def extract_malha_territorial():
+    pass
+
+def extract_clima():
+    pass
+
+
+raw_data = extract_uso_solo()
+raw_data.columns = raw_data.iloc[0]
+print(raw_data.columns)
