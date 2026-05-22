5560b62f784f90c383cbdc5b93b37de0da588c36
Maiko André <ewigemagie@outlook.com>
2026-05-22 10:05:20 -0300

    docs: update readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Análise de Dados Agrícolas
 
+## Tema: Expansão agrícola e impacto ambiental na Caatinga
+
 <img src='assets/Untitled-2026-05-13-0814.png' alt='arquitetura do projeto'>
 
 ## Projeto em andamento.
--- src/notebooks/notebook.py
+++ src/notebooks/notebook.py
@@ -8,6 +8,33 @@ app = marimo.App(width="medium")
 def _():
     import geopandas as gpd
 
+    return (gpd,)
+
+
+@app.cell
+def _(gpd):
+    df = gpd.read_file('data/BR_Municipios_2025/BR_Municipios_2025.shp')
+
+    df.head(10)
+    return (df,)
+
+
+@app.cell
+def _(df):
+    type(df)
+    return
+
+
+@app.cell
+def _(df):
+    guanambi = df.loc[df.NM_MUN.isin(['Guanambi'])].copy()
+    guanambi.head()
+    return (guanambi,)
+
+
+@app.cell
+def _(guanambi):
+    guanambi.plot()
     return
 
 
